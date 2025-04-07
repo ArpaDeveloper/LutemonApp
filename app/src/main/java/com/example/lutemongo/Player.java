d7072024a1dee3ce54ebf13a9abc1729a53eeff4
Eeli Ogata <eeli.ogata@gmail.com>
2025-04-07 19:00:29 +0300

    Started adding classes

---
+++ app/src/main/java/com/example/lutemongo/Player.java
@@ -0,0 +1,11 @@
+package com.example.lutemongo;
+
+public class Player {
+    public String name;
+    public Storage storage;
+    public Team team;
+
+    public void createLutemon() {
+
+    }
+}
