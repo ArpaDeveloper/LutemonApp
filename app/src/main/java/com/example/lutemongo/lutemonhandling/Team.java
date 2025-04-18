5cc58cbc229bfb95a92c7a2944f89a398b9a2bbb
Arpa <aarni.viljanen@gmail.com>
2025-04-18 19:45:41 +0300

    Changes

--- app/src/main/java/com/example/lutemongo/lutemonhandling/Team.java
+++
@@ -1,8 +0,0 @@
-package com.example.lutemongo.lutemonhandling;
-
-public class Team {
-    public Lutemon lutemon1;
-    public Lutemon lutemon2;
-    public Lutemon lutemon3;
-    private int position;
-}
