8e35ebb4efc992e1c4631900fd1f63111b7ec54c
Eeli Ogata <eeli.ogata@gmail.com>
2025-04-18 19:34:17 +0300

    removed xp gain from battles

--- app/src/main/java/com/example/lutemongo/actions/BattleSystem.java
+++ app/src/main/java/com/example/lutemongo/actions/BattleSystem.java
@@ -92,7 +92,7 @@ public class BattleSystem {
             // Determine winner
             if (teamLutemon.getHealth() > 0) {
                 battleLog.append(teamLutemon.getName()).append(" wins the battle!\n");
-                teamLutemon.setExperience(teamLutemon.getExperience() + 10);
+                // teamLutemon.setExperience(teamLutemon.getExperience() + 10);
             } else {
                 battleLog.append(enemyLutemon.getName()).append(" wins the battle!\n");
             }
