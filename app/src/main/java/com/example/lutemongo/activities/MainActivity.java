fdd68346332ed2cf076618c960bfc3c39388d4a9
Eeli Ogata <eeli.ogata@gmail.com>
2025-04-18 18:39:58 +0300

    cleaned up lines

--- app/src/main/java/com/example/lutemongo/activities/MainActivity.java
+++ app/src/main/java/com/example/lutemongo/activities/MainActivity.java
@@ -40,5 +40,4 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
-
 }
--- app/src/main/java/com/example/lutemongo/lutemonhandling/LutemonManager.java
+++ app/src/main/java/com/example/lutemongo/lutemonhandling/LutemonManager.java
@@ -70,5 +70,4 @@ public class LutemonManager {
             storage.setEnemyLutemon(enemyLutemon);
         }
     }
-
 }
